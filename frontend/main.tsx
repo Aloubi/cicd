983c68281fa1bdab6724589498b2daa8e2104a11
hibaaloubi <hiba.aloubi68@gmail.com>
2025-07-20 19:15:09 +0100

    commit of Todo App with Docker, Node.js, React, PostgreSQL

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './src/components/App.js'; // ← attention au .tsx si nécessaire
+import App from './src/components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(<App />);
